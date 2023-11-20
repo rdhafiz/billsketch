344cf322a3afa10f3a85436f55690b1e602f8e73
Noyon Ahammed <noyon.redishketch@gmail.com>
2023-11-20 15:24:15 +0600

    conflict resolved

--- resources/js/website/pages/home/home.vue
+++ resources/js/website/pages/home/home.vue
@@ -1,8 +1,7 @@
 <template>
     <Home_banner></Home_banner>
-
-    <Dummy_section :bg="'bg-white'" :title="'Features'"></Dummy_section>
-    <Dummy_section :bg="'bg-light border border-top border-bottom'" :title="'How It Works'"></Dummy_section>
+    <Home_features></Home_features>
+    <Home_works></Home_works>
     <Dummy_section :bg="'bg-white'" :title="'Testimonials'"></Dummy_section>
 
     <Cta :title="'Ready to Simplify Your Invoicing?'" :action_title="'Get Started'"></Cta>
@@ -13,9 +12,13 @@ import Dummy_section from "../layouts/dummy_section.vue";
 import Cta from "../layouts/cta.vue";
 
 import AOS from 'aos';
+import Home_features from "./widgets/home_features.vue";
+import Home_works from "./widgets/home_works.vue";
 
 export default {
     components: {
+        Home_works,
+        Home_features,
         Dummy_section,
         Home_banner,
         Cta
