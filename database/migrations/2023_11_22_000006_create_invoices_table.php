f87db43c2ebf80baedf527d8c2bfb0ca2f4fe5ab
Khalid Imran <ekhalid090@gmail.com>
2023-11-22 10:52:27 +0600

    invoice and invoice item migration

---
+++ database/migrations/2023_11_22_000006_create_invoices_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('client_id')->nullable();
+            $table->unsignedInteger('employee_id')->nullable();
+            $table->unsignedInteger('category_id');
+            $table->integer('invoice_no');
+            $table->timestamp('invoice_date')->nullable();
+            $table->timestamp('invoice_due_date')->nullable();
+            $table->tinyInteger('invoice_status')->nullable();
+            $table->text('cancel_reason')->nullable();
+            $table->text('overdue_reason')->nullable();
+            $table->string('currency');
+            $table->integer('recurring');
+            $table->integer('recurring_frequency')->nullable();
+            $table->float('sub_total')->nullable();
+            $table->float('tax')->nullable();
+            $table->float('discount')->nullable();
+            $table->float('bonus')->nullable();
+            $table->float('total')->nullable();
+            $table->text('note')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2023_11_22_000007_create_invoice_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('invoice_id');
+            $table->string('description')->nullable();
+            $table->string('unit_frequency')->nullable();
+            $table->float('unit_value')->nullable();
+            $table->float('total')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+};
