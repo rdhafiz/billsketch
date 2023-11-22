aa98db62e30518a48714607b45e3dac3bc9a22ca
Khalid Imran <ekhalid090@gmail.com>
2023-11-22 08:55:22 +0600

    category and user log migration

---
+++ database/migrations/2023_11_22_000005_create_user_activity_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('log_type');
+            $table->string('ip_address');
+            $table->string('device')->nullable();
+            $table->string('os')->nullable();
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_activity_logs');
+    }
+};
