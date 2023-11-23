b0294aaa17992dfd875d181da489a20459305113
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2023-11-23 16:20:25 +0600

    portal pages

---
+++ resources/js/portal/pages/clients/clients.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <h1>Clients</h1>
+
+</template>
+<script>
+export default {
+    components: {},
+    created() {
+        window.scroll(0, 0);
+    }
+}
+</script>
---
+++ resources/js/portal/pages/recurring-invoices/recurring-invoices.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <h1>Recurring Invoices</h1>
+
+</template>
+<script>
+export default {
+    components: {},
+    created() {
+        window.scroll(0, 0);
+    }
+}
+</script>
